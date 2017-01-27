26aca8430bb40b1eba9bddb514b35f184ad0cc04
JavierE195 <ellerbrockjavier@hotmail.com>
2017-01-27 10:29:31 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgePhase_IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string line1;
+
+            Console.Write("Insert your age: ");
+            line1 = Console.ReadLine();
+            age = int.Parse(line1);
+
+            if (age < 2)
+            {
+                Console.Write("You are a baby");
+            }
+
+            if ( age < 4 && age > 1)
+             {
+                Console.Write("You are a toddler");
+             }
+
+            if (age > 3 && age < 13)
+            {
+                Console.Write("You are a child");
+            }
+
+            if (age >12 && age < 18)
+            {
+                Console.Write("You are a teenager");
+            }
+
+            if (age >17 && age < 60)
+            {
+                Console.Write("You are an adult");
+            }
+
+            if (age >59 && age <200) 
+            {
+                Console.Write("You are an eldery");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
