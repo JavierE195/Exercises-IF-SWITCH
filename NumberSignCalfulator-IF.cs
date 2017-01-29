7fa34180e78955d1ef9b2da1147f2004b4ff0b3e
JavierE195 <ellerbrockjavier@hotmail.com>
2017-01-29 09:49:12 -0600

    Add files via upload

---
+++ NumberSignCalfulator-IF.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSignCalculator_IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal numberIn;
+            string line1;
+
+            Console.Write("\nInsert a number: ");
+            line1 = Console.ReadLine();
+            numberIn = decimal.Parse(line1);
+
+            if (numberIn != 0)
+            {
+                if (numberIn > 0)
+                {
+                    Console.Write("\nThe number is positive");
+                }
+                else
+                {
+                    Console.Write("\nThe number is negative");
+                }
+            }
+            else
+            {
+                Console.Write("\nThe number is zero");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
